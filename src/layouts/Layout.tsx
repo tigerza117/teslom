20a768903a1517267eb9e4363cc6f5b360735c59
tigerza117 <noppawanza1171@gmail.com>
2021-10-13 19:19:42 +0700

    fix: layout height

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,12 +6,22 @@ interface Props {}
 
 export const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div style={{backgroundImage:"url(" + "https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" + ")",
-    backgroundPosition: 'center',
-    backgroundSize:'cover',
-    backgroundRepeat:'no-repeat'}}>
-      <Navbar />
-      <MainButton />
+    <div style={{ minHeight: "100vh" }}>
+      <div
+        style={{
+          backgroundImage:
+            "url(" +
+            "https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" +
+            ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+        }}
+      >
+        <Navbar />
+        <MainButton />
+      </div>
     </div>
   );
 };
