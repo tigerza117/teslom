6b146872eb9cba06ac5d0152e34b0bfbb4a10a20
tigerza117 <noppawanza1171@gmail.com>
2021-10-13 23:25:52 +0700

    layout: Added drawer and refactor code

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Stack } from "@mui/material";
-import React from "react";
+import { Box, Button, Drawer, Stack } from "@mui/material";
+import React, { useState } from "react";
 import Logo from "../assets/images/logo.svg";
 
 const NavbarProducts = [
@@ -46,36 +46,82 @@ const NavbarPortal = [
 ];*/
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div style={{ width: "100vw" }}>
+      <React.Fragment>
+        <Drawer
+          anchor="right"
+          open={open}
+          onClose={() => {
+            setOpen(false);
+          }}
+        >
+          <Box width={300}>
+            <Button>Hello</Button>
+          </Box>
+        </Drawer>
+      </React.Fragment>
       <Box justifyContent="space-between" display="flex" padding="0 1rem">
         <Box>
           <img
             src={Logo}
-            style={{ width: "100%", height: "12.5px", padding: "20px"}}
+            style={{ width: "100%", height: "12.5px", padding: "20px" }}
           />
         </Box>
         <Box display="flex">
           <Stack direction="row" spacing={2}>
             {NavbarProducts.map(({ label }, index) => (
-              <Button size="large" style={{color: "#393c41", fontWeight: "600", textTransform:"capitalize"}} key={index}>
+              <Button
+                size="large"
+                style={{
+                  color: "#393c41",
+                  fontWeight: 600,
+                  textTransform: "capitalize",
+                }}
+                key={index}
+              >
                 {label}
               </Button>
             ))}
           </Stack>
         </Box>
-        <Box display="flex" >
-            <Stack direction="row" spacing={2} padding=".5rem 0">
-                <Button size="large" style={{color: "#393c41", fontWeight: "600", textTransform:"capitalize"}}>
-                    shop
-                </Button>
-                <Button size="large" style={{color: "#393c41", fontWeight: "600", textTransform:"capitalize"}}>
-                    account
-                </Button>
-                <Button size="large" style={{color: "#393c41", fontWeight: "600", textTransform:"capitalize"}}>
-                    button
-                </Button>
-            </Stack>
+        <Box display="flex">
+          <Stack direction="row" spacing={2} padding=".5rem 0">
+            <Button
+              size="large"
+              style={{
+                color: "#393c41",
+                fontWeight: 600,
+                textTransform: "capitalize",
+              }}
+            >
+              shop
+            </Button>
+            <Button
+              size="large"
+              style={{
+                color: "#393c41",
+                fontWeight: 600,
+                textTransform: "capitalize",
+              }}
+            >
+              account
+            </Button>
+            <Button
+              size="large"
+              style={{
+                color: "#393c41",
+                fontWeight: 600,
+                textTransform: "capitalize",
+              }}
+              onClick={() => {
+                setOpen(true);
+              }}
+            >
+              Menu
+            </Button>
+          </Stack>
         </Box>
       </Box>
     </div>
