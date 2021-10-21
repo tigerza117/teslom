724f9e918d18fc395fa3ddef3408d64336e54f77
tigerza117 <noppawanza1171@gmail.com>
2021-10-21 17:37:15 +0700

    sturecture: Added layout context to provider and use home view

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,37 +1,40 @@
-import { MainButton } from "./Button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "./Navbar";
 import { Box } from "@mui/system";
+import LayoutContext from "../contexts/LayoutContext";
 
-interface Props {}
+export const Layout: React.FC = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-export const Layout: React.FC<Props> = ({ children }) => {
+  const handleResize = () => {
+    const width = window.innerWidth;
+    setIsMobile(width < 1200);
+  };
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <Box sx={{ minHeight: "100vh" }}>
-      <Navbar />
-      <Session background="https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" />
-      <Session background="https://tesla-cdn.thron.com/delivery/public/image/tesla/325743a2-d187-4e28-91a3-5a2e47e317f7/bvlatuR/std/2880x1800/ModelX-Homepage-D" />
-      <Session background="https://tesla-cdn.thron.com/delivery/public/image/tesla/235e9193-899e-45f9-82d7-53837b32712e/bvlatuR/std/1226x562/lhd-interior-carousel-2-slide-3-desktop-1" />
-    </Box>
-  );
-};
-
-interface ISession {
-  background: string;
-}
-
-const Session: React.FC<ISession> = ({ background }) => {
-  return (
-    <Box
-      sx={{
-        backgroundImage: "url(" + background + ")",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        height: "100vh",
+    <LayoutContext.Provider
+      value={{
+        isMobile: isMobile,
+        isDrawerOpen: isDrawerOpen,
+        toggleDrawer: toggleDrawer,
       }}
     >
-      <MainButton />
-    </Box>
+      <Box sx={{ minHeight: "100vh" }}>
+        <Navbar />
+        {children}
+      </Box>
+    </LayoutContext.Provider>
   );
 };
--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -1,13 +1,112 @@
+import { Box, Button, Stack, styled, Typography } from "@mui/material";
 import React from "react";
-import MusicPlayerSlider from "../../components/home/MusicPlayerSlider";
+import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
 
-type Props = {};
+const MainTitle = styled(Typography)(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: "3rem",
+  marginBottom: 0,
+  color: "#393c41",
+  padding: 0,
+}));
 
-const Home: React.FC<Props> = () => {
+const MainSubTitle = styled("h5")(({ theme }) => ({
+  fontWeight: 200,
+  fontSize: "1rem",
+  "& a": {
+    color: "#393c41",
+  },
+}));
+
+const Home: React.FC = () => {
   return (
     <>
-      <MusicPlayerSlider />
+      <Section backgroundUrl="https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" />
+      <Section backgroundUrl="https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" />
     </>
+  );
+};
+
+interface ISession {
+  backgroundUrl: string;
+}
+
+const Section: React.FC<ISession> = ({ backgroundUrl }) => {
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${backgroundUrl})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          textAlign: "center",
+        }}
+      >
+        <Box>
+          <Stack direction="column" spacing={-1} sx={{ margin: "4rem 0" }}>
+            <MainTitle>Model 3</MainTitle>
+            <MainSubTitle>
+              Order Online for <a href="#">Touchless Delivery</a>
+            </MainSubTitle>
+          </Stack>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-around"
+          flexDirection="column"
+        >
+          <Box
+            sx={{
+              margin: "auto",
+            }}
+          >
+            <Stack direction="row" spacing={4} padding="1rem">
+              <Button
+                variant="contained"
+                style={{
+                  fontSize: ".8rem",
+                  padding: ".55rem 4.6rem",
+                  borderRadius: "25px",
+                  backgroundColor: "black",
+                  opacity: ".75",
+                  boxShadow: "none",
+                }}
+              >
+                CUSTOM ORDER
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  fontSize: ".8rem",
+                  padding: ".35rem 3.6rem",
+                  borderRadius: "25px",
+                  backgroundColor: "white",
+                  color: "black",
+                  opacity: ".75",
+                  boxShadow: "none",
+                }}
+              >
+                EXISTING INVENTORY
+              </Button>
+            </Stack>
+          </Box>
+          <span>
+            <ExpandMoreRoundedIcon
+              style={{ fontSize: "3.5rem", padding: ".6rem" }}
+            />
+          </span>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
