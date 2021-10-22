f4b2be625af7e02a102c307e319ac9520b09bd15
tigerza117 <noppawanza1171@gmail.com>
2021-10-22 17:59:20 +0700

    feat: Added section and animation to home page.

---
+++ src/constants/home.tsx
@@ -0,0 +1,20 @@
+export const ProductList = [
+  {
+    title: "Model S",
+    subTitle: "Order Online for Touchless Delivery",
+    image:
+      "https://tesla-cdn.thron.com/delivery/public/image/tesla/676da95d-942e-414c-8534-f1832f0d4ced/bvlatuR/std/3296x1798/ms-homepage-desktop",
+  },
+  {
+    title: "Model Y",
+    subTitle: "Order Online for Touchless Delivery",
+    image:
+      "https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0/bvlatuR/std/2880x2400/Desktop-ModelY?quality=70",
+  },
+  {
+    title: "Model 3",
+    subTitle: "Order Online for Touchless Delivery",
+    image:
+      "https://tesla-cdn.thron.com/delivery/public/image/tesla/e90a341e-f9ca-4aa1-8eab-94afea118786/bvlatuR/std/2880x1800/M3-Homepage-D",
+  },
+];
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,36 @@
 import { Box, Button, Stack, styled, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
 import gsap, { Power2 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ProductList } from "@constants/home";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const BackgroundWrapper = styled(Box)(({ theme }) => ({
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "100vh",
+}));
+
+const FixedWrapper = styled(Box)(({ theme }) => ({
+  height: "100%",
+  width: "100%",
+  position: "fixed",
+  opacity: 0,
+  top: 0,
+  display: "none",
+}));
+
+const TitleWrapper = styled(Box)(({ theme }) => ({
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  textAlign: "center",
+  margin: "auto",
+}));
 
 const MainTitle = styled(Typography)(({ theme }) => ({
   fontWeight: 600,
@@ -9,6 +38,7 @@ const MainTitle = styled(Typography)(({ theme }) => ({
   marginBottom: 0,
   color: "#393c41",
   padding: 0,
+  transform: "translateY(20px)",
 }));
 
 const MainSubTitle = styled("h5")(({ theme }) => ({
@@ -17,18 +47,20 @@ const MainSubTitle = styled("h5")(({ theme }) => ({
   "& a": {
     color: "#393c41",
   },
+  opacity: 0,
 }));
 
 const Home: React.FC = () => {
   return (
     <>
-      <Section backgroundUrl="https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" />
-      <Section backgroundUrl="https://tesla-cdn.thron.com/delivery/public/image/tesla/06af56e3-91c4-4bad-8d2e-0a24f64ec2a8/bvlatuR/std/2880x1800/MS-Exterior-Hero-Desktop" />
+      {ProductList.map(({ title, subTitle, image }) => (
+        <Section title={title} subTitle={subTitle} image={image} />
+      ))}
     </>
   );
 };
 
-const GoAway = styled(ExpandMoreRoundedIcon)(({ theme }) => ({
+const ExpandBtn = styled(ExpandMoreRoundedIcon)(({ theme }) => ({
   fontSize: "3.5rem",
   padding: ".6rem",
   animation: `loopUpDown 1000ms infinite`,
@@ -44,98 +76,115 @@ const GoAway = styled(ExpandMoreRoundedIcon)(({ theme }) => ({
   },
 }));
 
+const OrderBtn = styled(Button)(({ theme }) => ({
+  fontSize: ".8rem",
+  padding: ".55rem 4.6rem",
+  borderRadius: "25px",
+  backgroundColor: "black",
+  opacity: ".75",
+  boxShadow: "none",
+  color: "white",
+}));
+
+const InventoryBtn = styled(Button)(({ theme }) => ({
+  fontSize: ".8rem",
+  padding: ".35rem 3.6rem",
+  borderRadius: "25px",
+  backgroundColor: "white",
+  color: "black",
+  opacity: ".75",
+  boxShadow: "none",
+}));
+
 interface ISession {
-  backgroundUrl: string;
+  image: string;
+  title: string;
+  subTitle: string;
 }
 
-const Section: React.FC<ISession> = ({ backgroundUrl }) => {
-  const [state, setState] = useState({ yyy: "" });
-  let x = { counter: 0 };
+const Section: React.FC<ISession> = ({ image, title, subTitle }) => {
+  const refWrapper = useRef(null);
+  const refFixed = useRef(null);
+  const refTitle = useRef(null);
+  const refSubTitle = useRef(null);
+
   useEffect(() => {
-    gsap.timeline({ paused: false }).to(x, {
-      duration: 1,
-      counter: 3.1,
-      ease: Power2.easeIn,
-      onUpdate: () => {
-        setState({ yyy: x.counter.toFixed(1) });
+    gsap.to(refFixed.current, {
+      scrollTrigger: {
+        trigger: refWrapper.current,
+        scrub: true,
+        start: "40% 60%",
+        end: "60% 60%",
+        markers: false,
+        onEnter: () => {
+          console.log("e");
+        },
       },
+      display: "inline",
+      duration: 1,
+      opacity: 1,
+    });
+    gsap.to(refFixed.current, {
+      scrollTrigger: {
+        trigger: refWrapper.current,
+        scrub: true,
+        start: "40% 0%",
+        end: "60% 0%",
+        markers: false,
+      },
+      opacity: 0,
+    });
+    gsap.to(refTitle.current, {
+      duration: 0.5,
+      east: Power2.easeIn,
+      transform: "translateY(0px)",
+      delay: 1,
+    });
+    gsap.to(refSubTitle.current, {
+      duration: 0.8,
+      opacity: 1,
+      delay: 2,
     });
     return () => {};
   }, []);
 
   return (
-    <Box
+    <BackgroundWrapper
       sx={{
-        backgroundImage: `url(${backgroundUrl})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        height: "100vh",
+        backgroundImage: `url(${image})`,
       }}
+      ref={refWrapper}
     >
-      <Box
-        sx={{
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          textAlign: "center",
-        }}
-      >
-        <Box>
-          <Stack direction="column" spacing={-1} sx={{ margin: "4rem 0" }}>
-            <MainTitle>Model {state.yyy}</MainTitle>
-            <MainSubTitle>
-              Order Online for <a href="#">Touchless Delivery</a>
-            </MainSubTitle>
-          </Stack>
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="space-around"
-          flexDirection="column"
-        >
-          <Box
-            sx={{
-              margin: "auto",
-            }}
-          >
-            <Stack direction="row" spacing={4} padding="1rem">
-              <Button
-                variant="contained"
-                style={{
-                  fontSize: ".8rem",
-                  padding: ".55rem 4.6rem",
-                  borderRadius: "25px",
-                  backgroundColor: "black",
-                  opacity: ".75",
-                  boxShadow: "none",
-                }}
-              >
-                CUSTOM ORDER
-              </Button>
-              <Button
-                variant="contained"
-                style={{
-                  fontSize: ".8rem",
-                  padding: ".35rem 3.6rem",
-                  borderRadius: "25px",
-                  backgroundColor: "white",
-                  color: "black",
-                  opacity: ".75",
-                  boxShadow: "none",
-                }}
-              >
-                EXISTING INVENTORY
-              </Button>
+      <FixedWrapper ref={refFixed}>
+        <TitleWrapper>
+          <Box>
+            <Stack direction="column" spacing={-1} sx={{ margin: "4rem 0" }}>
+              <MainTitle ref={refTitle}>{title}</MainTitle>
+              <MainSubTitle ref={refSubTitle}>{subTitle}</MainSubTitle>
             </Stack>
           </Box>
-          <span>
-            <GoAway />
-          </span>
-        </Box>
-      </Box>
-    </Box>
+          <Box
+            display="flex"
+            justifyContent="space-around"
+            flexDirection="column"
+          >
+            <Box
+              sx={{
+                margin: "auto",
+              }}
+            >
+              <Stack direction="row" spacing={4} padding="1rem">
+                <OrderBtn>CUSTOM ORDER</OrderBtn>
+                <InventoryBtn>EXISTING INVENTORY</InventoryBtn>
+              </Stack>
+            </Box>
+            <Box>
+              <ExpandBtn />
+            </Box>
+          </Box>
+        </TitleWrapper>
+      </FixedWrapper>
+    </BackgroundWrapper>
   );
 };
 
