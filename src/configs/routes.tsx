23d3333738ab47ee63f057efebe7d6b89027b853
tigerza117 <noppawanza1171@gmail.com>
2021-10-24 01:17:28 +0700

    views: Model S

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,10 +1,14 @@
 import React, { Suspense } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { Layout } from "@layouts/Layout";
+import ModelS from "@views/product/ModelS";
 
 const Home = React.lazy(() => import("@views/home/Home"));
 
-const publicPaths = [{ exact: true, path: "/", component: Home }];
+const publicPaths = [
+  { exact: true, path: "/", component: Home },
+  { exact: true, path: "/models", component: ModelS },
+];
 
 const publicRoutes = publicPaths.map(({ path, ...props }) => (
   <Route key={path} path={path} {...props} />
---
+++ src/views/product/ModelS.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ModelS() {
+  return <div>Hello! ModelS.</div>;
+}
+
+export default ModelS;
