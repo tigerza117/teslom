04454158168cc6e4a080f157cd5a97210a41ef0b
santhitak <santhita.sk@gmail.com>
2021-10-25 14:44:03 +0700

    style: LinkBtn and add margin

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -5,10 +5,13 @@ const Wrapper = styled(Box)(({ theme }) => ({
   transformOrigin: "bottom",
   bottom: 0,
   width: "100%",
+  margin: "20px 0",
 }));
 
 const LinkBtn = styled(Button)(({ theme }) => ({
-  color: "#5D5C61",
+  color: "#393c41",
+  fontSize: ".75rem",
+  textTransform: "capitalize",
 }));
 
 const ResponsiveLinkBtn = styled(LinkBtn)(({ theme }) => ({
@@ -20,9 +23,9 @@ const ResponsiveLinkBtn = styled(LinkBtn)(({ theme }) => ({
 export default function Footer() {
   return (
     <Wrapper>
-      <Box sx={{ display: "flex" }}>
-        <Stack direction={{ xs: "column", md: "row" }} sx={{ m: "auto" }}>
-          <LinkBtn>Tesla 2021</LinkBtn>
+      <Box sx={{ display: "flex"}}>
+        <Stack direction={{ xs: "column", md: "row" }} sx={{ m: "auto"}}>
+          <LinkBtn>Tesla &copy; 2021</LinkBtn>
           <LinkBtn>Privacy & Legal</LinkBtn>
           <ResponsiveLinkBtn>Contact</ResponsiveLinkBtn>
           <LinkBtn>Careers</LinkBtn>
