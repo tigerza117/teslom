9ce3b03e92d0bde4bac8f9d066054697c3976fed
santhitak <santhita.sk@gmail.com>
2021-10-26 19:02:16 +0700

    feat: Model3

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -2,12 +2,14 @@ import React, { Suspense } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { Layout } from "@layouts/Layout";
 import ModelS from "@views/product/ModelS";
+import Model3 from "@views/product/Model3";
 
 const Home = React.lazy(() => import("@views/home/Home"));
 
 const publicPaths = [
   { exact: true, path: "/", component: Home },
   { exact: true, path: "/models", component: ModelS },
+  { exact: true, path: "/model3", component: Model3 },
 ];
 
 const publicRoutes = publicPaths.map(({ path, ...props }) => (
---
+++ src/views/product/Model3.tsx
@@ -0,0 +1,200 @@
+import styled from "@emotion/styled";
+import { ImageAspectRatio } from "@mui/icons-material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const BackgroundWrapper = styled(Box)(({ theme }) => ({
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "100vh",
+}));
+
+const TitleWrapper = styled(Box)(({ theme }) => ({
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  textAlign: "center",
+  margin: "auto",
+}));
+
+const MainTitle = styled(Typography)(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: "2.5rem",
+  marginBottom: 0,
+  color: "#181b21",
+  padding: 0,
+  transform: "translateY(20px)",
+}));
+
+const MainSubTitle = styled("h5")(({ theme }) => ({
+  fontWeight: 200,
+  fontSize: "1.2rem",
+  padding: "1.3rem 0",
+}));
+
+const FeatureTitle = styled(Typography)(({ theme }) => ({
+  color: "white",
+  fontWeight: 600,
+  fontSize: "1.8rem",
+}));
+
+const FeatureSubTitle = styled(Typography)(({ theme }) => ({
+  color: "white",
+  fontWeight: 500,
+  fontSize: "0.8rem",
+}));
+
+const OutlineBtnWhite = styled(Button)(({ theme }) => ({
+  fontSize: ".8rem",
+  padding: ".3rem 2.5rem",
+  borderRadius: "25px",
+  border: "3px solid white",
+  boxShadow: "none",
+  color: "white",
+  marginTop: ".8rem",
+}));
+
+const OutlineBtnBlack = styled(Button)(({ theme }) => ({
+  fontSize: ".8rem",
+  padding: ".3rem 2.5rem",
+  borderRadius: "25px",
+  border: "3px solid black",
+  boxShadow: "none",
+  color: "black",
+  marginTop: ".8rem",
+}));
+
+const AdsTitle = styled(Typography)(({ theme }) => ({
+  color: "white",
+  fontWeight: 600,
+  fontSize: "1.8rem",
+  margin: "auto 0",
+  "&::after": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    width: "100%",
+    height: "40vh",
+    marginTop: "2vh",
+    background: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgb(0, 0, 0, 0))",
+  },
+}));
+
+const InfoFeature = styled(Box)(({ theme }) => ({
+  margin: "auto",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "5rem 0",
+}));
+
+const GalleryContainer = styled(Box)(({ theme }) => ({
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "45vh",
+  width: "80vh",
+}));
+
+const InfoContainer = styled(Box)(({ theme }) => ({
+  height: "45vh",
+  width: "80vh",
+  verticalAlign: "middle",
+}));
+
+const InfoTitle = styled(Typography)(({ theme }) => ({
+  color: "white",
+  fontWeight: 700,
+  fontSize: ".9rem",
+  padding: "6rem 0 0 0",
+}));
+
+const InfoSubTitle = styled(Typography)(({ theme }) => ({
+  color: "white",
+  fontWeight: 500,
+  fontSize: "0.9rem",
+}));
+
+const MainTitleContent = styled(Typography)(({ theme }) => ({
+  fontWeight: 600,
+  fontSize: "1.8rem",
+  margin: 0,
+  color: "#181b21",
+  padding: 0,
+}));
+
+const SubTitleContent = styled("h5")(({ theme }) => ({
+  fontWeight: 200,
+  fontSize: "1.2rem",
+  margin: "0",
+  padding: "0",
+}));
+
+const SubContent = styled("h5")(({ theme }) => ({
+  fontWeight: 400,
+  fontSize: "1rem",
+  margin: "0",
+  padding: "0",
+  color: "#393c41",
+}));
+
+function Model3() {
+  return (
+    <>
+      <BackgroundWrapper
+        sx={{
+          backgroundImage: `url(https://tesla-cdn.thron.com/delivery/public/image/tesla/32e5e0f3-5c04-42ef-8f8f-c6b1c26f8a9e/bvlatuR/std/2880x1800/ms-main-hero-desktop)`,
+        }}
+      >
+        <TitleWrapper>
+          <Box sx={{ marginTop: "12vh" }}>
+            <Stack direction="column" spacing={-1}>
+              <MainTitle>Model S</MainTitle>
+              <MainSubTitle>Plaid</MainSubTitle>
+            </Stack>
+          </Box>
+          <Box sx={{ margin: "0 auto", padding: "1.8rem 0" }}>
+            <Stack direction="row" spacing={8}>
+              <Box>
+                <Stack direction="column" spacing={1}>
+                  <FeatureTitle>396 mi</FeatureTitle>
+                  <FeatureSubTitle>Range (EPA est.)</FeatureSubTitle>
+                </Stack>
+              </Box>
+              <Box>
+                <Stack direction="column" spacing={1}>
+                  <FeatureTitle>1.99 s</FeatureTitle>
+                  <FeatureSubTitle>0-60 mph*</FeatureSubTitle>
+                </Stack>
+              </Box>
+              <Box>
+                <Stack direction="column" spacing={1}>
+                  <FeatureTitle>200 mph</FeatureTitle>
+                  <FeatureSubTitle>Top Speed</FeatureSubTitle>
+                </Stack>
+              </Box>
+              <Box>
+                <Stack direction="column" spacing={1}>
+                  <FeatureTitle>1,020 hp</FeatureTitle>
+                  <FeatureSubTitle>Peak Power</FeatureSubTitle>
+                </Stack>
+              </Box>
+              <Box>
+                <OutlineBtnWhite>ORDER NOW</OutlineBtnWhite>
+              </Box>
+            </Stack>
+          </Box>
+        </TitleWrapper>
+      </BackgroundWrapper>
+      <BackgroundWrapper
+        sx={{
+          backgroundImage: `url(https://tesla-cdn.thron.com/delivery/public/image/tesla/32e5e0f3-5c04-42ef-8f8f-c6b1c26f8a9e/bvlatuR/std/2880x1800/ms-main-hero-desktop)`,
+        }}
+      ></BackgroundWrapper>
+    </>
+  );
+}
+
+export default Model3;
