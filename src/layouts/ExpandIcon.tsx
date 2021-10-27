72a85f4ea8e7124f67d278a115471fc80429633a
santhitak <santhita.sk@gmail.com>
2021-10-27 21:10:09 +0700

    feat: ExpandIcon

---
+++ src/layouts/ExpandIcon.tsx
@@ -0,0 +1,24 @@
+import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
+import { Box, styled } from "@mui/material";
+
+const ExpandBtn = styled(ExpandMoreRoundedIcon)(({ theme }) => ({
+    fontSize: "3.5rem",
+    padding: ".2rem",
+    animation: `loopUpDown 1000ms infinite`,
+    "@keyframes loopUpDown": {
+      "80%": {
+        opacity: 1,
+        transform: "translate3d(0,10px,0)",
+      },
+      "100%": {
+        opacity: 1,
+        transform: "translate3d(0,0px,0)",
+      },
+    },
+  }));
+
+export default function Expanding() {
+    return (
+        <Box><ExpandBtn /></Box>
+    );
+}
