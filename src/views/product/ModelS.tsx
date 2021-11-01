d97c10549e2352e8f3767d7a4a92aa186e8feec0
santhitak <santhita.sk@gmail.com>
2021-11-02 00:41:14 +0700

    feat: last page done

--- src/views/product/ModelS.tsx
+++ src/views/product/ModelS.tsx
@@ -1224,16 +1224,32 @@ function ModelS() {
           </Stack>
         </Box>
       </BackgroundWrapper>
-      <BackgroundWrapper style={{ backgroundColor: "white" }}>
-        <img
-          src="https://tesla-cdn.thron.com/delivery/public/image/tesla/1c9e3fa8-7eba-4d75-bf00-03a3dbdd6d06/bvlatuR/std/1660x920/MS-Order-Hero-Desktop"
-          loading="lazy"
-          style={{
-            width: "50vw",
-            height: "100%",
-            objectFit: "cover",
-          }}
-        />
+      <BackgroundWrapper style={{ backgroundColor: "white"}}>
+        <Box style={{width: "100%", height: "100%", display: "flex", justifyContent: "center" }}>
+          <Stack direction="row" style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <Box alignItems="center" margin="auto" padding="0 1rem 0 10rem ">
+              <Stack spacing={6}>
+                <MainTitleContent>Model S</MainTitleContent>
+                <Box>
+                  <Stack direction="row" spacing={4}>
+                    <OutlineBtnBlack style={{margin: "0", padding:".4rem 4rem"}}>order now</OutlineBtnBlack>
+                    <OutlineBtnBlack style={{padding:".4rem 5.5rem"}}>compare</OutlineBtnBlack>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Box>
+            <img
+              src="https://tesla-cdn.thron.com/delivery/public/image/tesla/1c9e3fa8-7eba-4d75-bf00-03a3dbdd6d06/bvlatuR/std/1660x920/MS-Order-Hero-Desktop"
+              loading="lazy"
+              style={{
+                width: "50vw",
+                height: "100%",
+                objectFit: "contain",
+                float: "right",
+              }}
+            />
+          </Stack>
+        </Box>
       </BackgroundWrapper>
     </>
   );
