3b506f46e01d1aec9f340f331fb3b3ba5b22634f
AnDelXis <63070135@it.kmitl.ac.th>
2021-11-10 18:55:38 +0700

    -Page update

--- src/constants/cybertruckSlider.tsx
+++ src/constants/cybertruckSlider.tsx
@@ -1,20 +1,32 @@
 export const CybertruckPictureSlider = [
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/05_Desktop.jpg"
-    },
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/06_Desktop.jpg"
-    },
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/07_Desktop.jpg"
-    },
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/08_Desktop.jpg"
-    },
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/09_Desktop.jpg"
-    },
-    {
-        image: "https://www.tesla.com/xNVh4yUEc3B9/10_Desktop.jpg"
-    },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/05_Desktop.jpg",
+    txt: "a",
+    title: "aa",
+  },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/06_Desktop.jpg",
+    txt: "b",
+    title: "bb",
+  },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/07_Desktop.jpg",
+    txt: "c",
+    title: "cc",
+  },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/08_Desktop.jpg",
+    txt: "d",
+    title: "dd",
+  },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/09_Desktop.jpg",
+    txt: "e",
+    title: "ee",
+  },
+  {
+    image: "https://www.tesla.com/xNVh4yUEc3B9/10_Desktop.jpg",
+    txt: "f",
+    title: "ff",
+  },
 ];
