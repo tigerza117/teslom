569afa22b51d8089e63e38c2768f6c3c39480247
santhitak <santhita.sk@gmail.com>
2021-11-16 03:36:17 +0700

    remove: container

--- src/views/product/ModelX.tsx
+++ src/views/product/ModelX.tsx
@@ -13,10 +13,9 @@ import {
   Feature,
   Specs,
   OrderNow,
+  AllNewInterior
 } from "@components/model_x/section";
-import { Container } from "@components/shared/Container";
-import { BackgroundWrapper, TitleWrapper } from "@components/shared/Wrapper";
-import { AdsTitle } from "@components/shared/Title";
+import { BackgroundWrapper } from "@components/shared/Wrapper";
 import { useLayoutContext } from "@contexts/LayoutContext";
 import { useEffect } from "react";
 
@@ -30,18 +29,9 @@ function ModelX() {
   }, []);
 
   return (
-    <Container>
+    <>
       <HeroSection />
-      <BackgroundWrapper style={{ backgroundColor: "black", height: "15vh" }}>
-        <TitleWrapper>
-          <AdsTitle>All-New Interior</AdsTitle>
-        </TitleWrapper>
-      </BackgroundWrapper>
-      <BackgroundWrapper
-        sx={{
-          backgroundImage: `url(https://tesla-cdn.thron.com/delivery/public/image/tesla/177c0faf-b852-47ea-a218-394368e15a20/bvlatuR/std/2880x1800/MS-Interior-Hero-Desktop)`,
-        }}
-      ></BackgroundWrapper>
+      <AllNewInterior />
       <BackgroundWrapper
         style={{ backgroundColor: "black" }}
       ></BackgroundWrapper>
@@ -58,7 +48,7 @@ function ModelX() {
       <Feature />
       <Specs />
       <OrderNow />
-    </Container>
+    </>
   );
 }
 
