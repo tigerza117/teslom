86cf7249560c81cea1b2d17bbc4fd0b0aa39acd9
santhitak <santhita.sk@gmail.com>
2021-11-15 17:51:02 +0700

    add: text on vdo - model 3

--- src/components/model_3/AutopilotSection.tsx
+++ src/components/model_3/AutopilotSection.tsx
@@ -4,68 +4,31 @@ import {
   MainTitleContent,
   SubContent,
   SubTitleContent,
-  VdoOverlayText
+  VdoOverlayText,
 } from "@components/shared/Title";
 import { BtnOutline } from "@components/shared/Button";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import VidsOverlay from "@components/shared/VdoOverlay";
 
 export function Autopilot() {
   return (
     <>
       <BackgroundWrapper style={{ backgroundColor: "#f5f5f5" }}>
         <Box>
-        <video
-          src="https://tesla-cdn.thron.com/delivery/public/video/tesla/65011bbb-6f1c-422e-9e7a-727ba24bad15/bvlatuR/WEBHD/autopilot"
-          style={{
-            width: "100vw",
-            height: "75vh",
-            objectFit: "cover",
-            zIndex: -1,
-          }}
-          autoPlay
-          loop
-          preload="auto"
-        />
-        <VdoOverlay>
-            <Stack direction="column" spacing={3}>
-              <Box zIndex={1}>
-                <MainTitleContent>360°</MainTitleContent>
-                <VdoOverlayText>
-                  Rear, side and forward-facing cameras provide maximum
-                  visibility
-                </VdoOverlayText>
-              </Box>
-              <Box zIndex={1}>
-                <MainTitleContent>250 m</MainTitleContent>
-                <VdoOverlayText>
-                  Powerful visual processing at up to 250 meters of range
-                </VdoOverlayText>
-              </Box>
-              <Box zIndex={1}>
-                <Stack direction="row" spacing={1}>
-                  <img
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      display: "inline",
-                      marginBottom: "8px",
-                    }}
-                    src="https://tesla-cdn.thron.com/delivery/public/image/tesla/0244c34f-2054-4549-9aff-a055db265926/bvlatuR/std/0x0/autopilot"
-                    alt=""
-                  />
-                  <MainTitleContent style={{ fontSize: ".9rem" }}>
-                    Ultrasonic
-                    <br />
-                    Sensors
-                  </MainTitleContent>
-                </Stack>
-                <VdoOverlayText>
-                  Detects nearby cars, helps prevent potential collisions and
-                  assists with parking
-                </VdoOverlayText>
-              </Box>
-            </Stack>
-          </VdoOverlay>
+          <video
+            src="https://tesla-cdn.thron.com/delivery/public/video/tesla/65011bbb-6f1c-422e-9e7a-727ba24bad15/bvlatuR/WEBHD/autopilot"
+            style={{
+              width: "100vw",
+              height: "75vh",
+              objectFit: "cover",
+              zIndex: -1,
+            }}
+            autoPlay
+            loop
+            preload="auto"
+          />
+
+          <VidsOverlay />
         </Box>
         <Box padding="1rem 0 0 0">
           <Stack
