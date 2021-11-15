cefc86e8e766a8bf4b8aa7ae92e786b0e6e606d5
santhitak <santhita.sk@gmail.com>
2021-11-15 17:51:11 +0700

    add: text on img - model 3

--- src/components/model_3/InteriorSection.tsx
+++ src/components/model_3/InteriorSection.tsx
@@ -1,13 +1,13 @@
 import { Box, Stack } from "@mui/material";
-import { BackgroundWrapper, VdoOverlay } from "@components/shared/Wrapper";
+import { BackgroundWrapper } from "@components/shared/Wrapper";
 import {
   MainTitleContent,
   SubContent,
   SubTitleContent,
-  VdoOverlayText,
 } from "@components/shared/Title";
 import { BtnOutline } from "@components/shared/Button";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import PicsOverlay from "@components/shared/ImgOverlay";
 
 export function Interior() {
   return (
@@ -23,55 +23,7 @@ export function Interior() {
               objectFit: "cover",
             }}
           />
-          <Box>
-            <VdoOverlay style={{ float: "right", paddingRight: "1rem" }}>
-              <Stack direction="column" spacing={3}>
-                <Box zIndex={1} style={{ color: "white" }}>
-                  <MainTitleContent style={{ color: "white" }}>
-                  15
-inch
-                  </MainTitleContent>
-                  <VdoOverlayText>
-                  A touchscreen display designed to improve over time
-                  </VdoOverlayText>
-                </Box>
-                <Box zIndex={1} style={{ color: "white" }}>
-                <Stack direction="row" spacing={1}>
-                    <img
-                      style={{
-                        width: "40px",
-                        height: "40px",
-                        display: "inline",
-                        marginBottom: "8px",
-                      }}
-                      src="https://tesla-cdn.thron.com/delivery/public/image/tesla/863c27b1-9268-486c-ab6a-b807d0e7d096/bvlatuR/std/0x0/wifi"
-                      alt=""
-                    />
-                  </Stack>
-                  <VdoOverlayText>
-                  Over-the-air software updates introduce new features, functionality and performance
-                  </VdoOverlayText>
-                </Box>
-                <Box zIndex={1} style={{ color: "white" }}>
-                  <Stack direction="row" spacing={1}>
-                    <img
-                      style={{
-                        width: "auto",
-                        height: "40px",
-                        display: "inline",
-                        marginBottom: "8px",
-                      }}
-                      src="https://tesla-cdn.thron.com/delivery/public/image/tesla/af5593f2-3cc8-40b1-8063-d3f4631276a0/bvlatuR/std/4096x1905/model-3-interior-hero-roof-badge"
-                      alt=""
-                    />
-                  </Stack>
-                  <VdoOverlayText>
-                  An expansive Glass Roof provides more headroom and UV protection
-                  </VdoOverlayText>
-                </Box>
-              </Stack>
-            </VdoOverlay>
-          </Box>
+          <PicsOverlay />
         </Box>
         <Box padding="1.5rem 0 0 0">
           <Stack
