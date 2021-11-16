9ef5cdde9aa37c63edb227211e1c2dd27a212758
santhitak <santhita.sk@gmail.com>
2021-11-16 12:30:00 +0700

    Revert "Add text at last page"
    This reverts commit b42fd1f171fd48e17cf90b526faa4c1ce742aa60.

--- src/constants/cybertruckSlider.tsx
+++ src/constants/cybertruckSlider.tsx
@@ -1,47 +1,32 @@
 export const CybertruckPictureSlider = [
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/05_Desktop.jpg",
-    txt: {
-      subtitle: "With up to 3,500 pounds of payload capacity and adjustable air suspension, Cybertruck is the most powerful tool we have ever built, " +
-          "engineered with 100 cubic feet of exterior, lockable storage — including a magic tonneau cover that is strong enough to stand on."
-    },
+    txt: "a",
+    title: "aa",
   },
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/06_Desktop.jpg",
-    txt: {
-      title: "VAULT-LIKE STORAGE",
-      subtitle: "Space for your toolbox, tire and Cyberquad, with room to spare. Utilize 100 cubic feet of exterior, " +
-          "lockable storage — including the under-bed, frunk and sail pillars."
-    },
+    txt: "b",
+    title: "bb",
   },
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/07_Desktop.jpg",
-    txt: {
-      title: "RUGGED STRENGTH",
-      subtitle: "With the ability to pull near infinite mass and a towing capability of over 14,000 pounds, Cybertruck can perform in almost any extreme situation with ease."
-    },
+    txt: "c",
+    title: "cc",
   },
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/08_Desktop.jpg",
-    txt: {
-      title: "ADAPTIVE AIR SUSPENSION",
-      subtitle: "Raise and lower suspension four inches in either direction for easy access to Cybertruck or the vault, " +
-          "while self-leveling capabilities adapt to any occasion and assist with every job."
-    },
+    txt: "d",
+    title: "dd",
   },
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/09_Desktop.jpg",
-    txt: {
-      title: "FLEXIBLE INTERIOR",
-      subtitle: "Seat six comfortably with additional storage under the second-row seats. Complete with an advanced 17” touchscreen with an all-new customized user interface."
-    },
+    txt: "e",
+    title: "ee",
   },
   {
     image: "https://www.tesla.com/xNVh4yUEc3B9/10_Desktop.jpg",
-    txt: {
-      title: "AWESOME ADAPTABILITY",
-      subtitle: "From rugged to refined, Cybertruck is completely adaptable for your needs. " +
-          "Prepare for every experience with a versatile utilitarian design — including on-board power and compressed air."
-    },
+    txt: "f",
+    title: "ff",
   },
 ];
