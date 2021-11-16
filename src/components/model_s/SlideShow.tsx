3179f2ea41c3aae60d5f6dcc21dfb2b62d059084
santhitak <santhita.sk@gmail.com>
2021-11-16 18:02:56 +0700

    add: slider plugin

--- src/components/model_s/SlideShow.tsx
+++ src/components/model_s/SlideShow.tsx
@@ -1,6 +1,14 @@
 import { Box, Stack } from "@mui/material";
 import { BackgroundWrapper, SliderAdjust } from "@components/shared/Wrapper";
 import { MainTitleContent, FeatureSubTitle } from "@components/shared/Title";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  { url: "https://tesla-cdn.thron.com/delivery/public/image/tesla/82d678c3-1d60-4827-887b-eb44bbac528f/bvlatuR/std/2442x1124/MS-Interior-Carousel-A-Desktop" },
+  { url: "https://tesla-cdn.thron.com/delivery/public/image/tesla/bb2fe12c-36d1-4e56-957c-cd6c1b8a8f40/bvlatuR/std/2442x1124/MS-Interior-Carousel-B-Desktop" },
+  { url: "https://tesla-cdn.thron.com/delivery/public/image/tesla/118a3cee-9850-437d-b9e8-e88695d58906/bvlatuR/std/2442x1124/MS-Interior-Carousel-C-Desktop" },
+  { url: "https://tesla-cdn.thron.com/delivery/public/image/tesla/8dfd0c9e-45d5-4782-a071-c597c372429f/bvlatuR/std/2442x1124/MS-Interior-Carousel-D-Desktop" },
+];
 
 export function SlideShow() {
   return (
@@ -16,10 +24,12 @@ export function SlideShow() {
                 alignItems: "center",
               }}
             >
-              <img
-                style={{ width: "67vw" }}
-                src="https://tesla-cdn.thron.com/delivery/public/image/tesla/8dfd0c9e-45d5-4782-a071-c597c372429f/bvlatuR/std/2442x1124/MS-Interior-Carousel-D-Desktop"
-                alt=""
+              <SimpleImageSlider
+                width={"65vw"}
+                height={504}
+                images={images}
+                showBullets={true}
+                showNavs={true}
               />
               <Box width="50%" padding="1.2rem">
                 <Stack direction="column" spacing={2}>
