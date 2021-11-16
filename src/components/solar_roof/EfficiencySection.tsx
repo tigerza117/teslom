5e80dc50b387862848cc793badccc515b164e91e
santhitak <santhita.sk@gmail.com>
2021-11-17 04:58:39 +0700

    refactor: efficiency

--- src/components/solar_roof/EfficiencySection.tsx
+++ src/components/solar_roof/EfficiencySection.tsx
@@ -6,57 +6,32 @@ import {
   SubTitleContent,
 } from "@components/shared/Title";
 import { BtnOutline } from "@components/shared/Button";
+import { HorizonContent } from "@layouts/HoriContent";
+import { HoriImg } from "@components/shared/Container";
 
 export function Efficiency() {
   return (
     <>
-      <BackgroundWrapper style={{ backgroundColor: "#f5f5f5" }}>
+      <BackgroundWrapper style={{ backgroundColor: "white" }}>
         <Box>
           <Stack direction="row">
-            <Box>
-              <Stack
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box
-                  sx={{
-                    padding: "3rem 1.5rem",
-                  }}
-                >
-                  <SubTitleContent>Efficiency</SubTitleContent>
-                  <MainTitleContent>Maximum Solar Production</MainTitleContent>
-                  <SubContent style={{ padding: "1rem 0" }}>
+            <HorizonContent>
+              <SubTitleContent>Efficiency</SubTitleContent>
+              <MainTitleContent>Maximum Solar Production</MainTitleContent>
+              <SubContent style={{ padding: "1rem 0" }}>
                     Generate the most energy possible, even on roofs with
                     complicated angles and intermittent sunlight. Combined with
                     Tesla Solar Inverter, your fully integrated system is safe,
                     reliable and outage ready. With built-in connectivity, Tesla
                     Solar Inverter will continue to improve with each
                     over-the-air software update.
-                  </SubContent>
-                </Box>
-                <Box
-                  style={{
-                    padding: "10rem 0 0 1.5rem",
-                  }}
-                >
-                  <BtnOutline style={{ margin: "0" }}>ORDER NOW</BtnOutline>
-                </Box>
-              </Stack>
-            </Box>
+              </SubContent>
+            </HorizonContent>
             <Box>
-              <img
+              <HoriImg
                 src="https://www.tesla.com/sites/default/files/solarroof/v3/inverter/Inverter_V8_D.jpg"
                 alt=""
                 loading="lazy"
-                style={{
-                  width: "73vw",
-                  height: "100vh",
-                  objectFit: "cover",
-                  float: "right",
-                }}
               />
             </Box>
           </Stack>
